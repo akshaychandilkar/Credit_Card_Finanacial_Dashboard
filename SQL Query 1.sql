7a0e8ea10d8565df1d8b7a3439011070849ab66e
Akshay Pandurang Chandilkar <150008953+akshaychandilkar@users.noreply.github.com>
2025-05-16 14:52:52 +0530

    Add files via upload

---
+++ SQL Query 1.sql
@@ -0,0 +1,35 @@
+CREATE TABLE cc_detail (
+    Client_Num INT,
+    Card_Category VARCHAR(20),
+    Annual_Fees INT,
+    Activation_30_Days INT,
+    Customer_Acq_Cost INT,
+    Week_Start_Date DATE,
+    Week_Num VARCHAR(20),
+    Qtr VARCHAR(10),
+    current_year INT,
+    Credit_Limit DECIMAL(10,2),
+    Total_Revolving_Bal INT,
+    Total_Trans_Amt INT,
+    Total_Trans_Ct INT,
+    Avg_Utilization_Ratio DECIMAL(10,3),
+    Use_Chip VARCHAR(10),
+    Exp_Type VARCHAR(50),
+    Interest_Earned DECIMAL(10,3),
+    Delinquent_Acc VARCHAR(5)
+);
+
+
+select * from cc_detail
+
+SET datestyle = 'DMY';
+
+COPY cc_detail
+FROM 'C:\Credit Card Customer Report\credit_card.csv' 
+delimiter ','
+CSV HEADER;
+
+COPY cc_detail 
+FROM 'C:\Credit Card Customer Report\cc_add.csv' 
+delimiter ','
+CSV HEADER;
